ea17e2b46ec8f452b545c03d34785b7c4ec57f3d
Hailey J Gaylor <48165448+Hgaylor0220@users.noreply.github.com>
2019-09-16 23:02:46 -0700

    completed test for constructors

--- spec/date-spec.js
+++ spec/date-spec.js
@@ -1,12 +1,85 @@
  import { GalacticCalculator } from './../src/galactic-cal.js'
 
  describe('GalacticCalculator', function() {
-     //first test to fail
+     //first test to pass
      it('should expect to fail/if it returns correct value of the age', function() {
-       let age = 10;
-       var galacticCalulation = new GalacticCalculator(age);
-       console.log(age);
+       let age = 35;
+       const galacticCalulation = new GalacticCalculator(age);
 
-       expect(galacticCalulation.age).toEqual("23")
+
+       expect(galacticCalulation.age).toEqual("age")
      })
    })
+
+ describe('GalacticCalculator', function() {
+
+     it('should expect to pass/if it returns correct value of the age', function() {
+       let age = 35;
+       const galacticCalulation = new GalacticCalculator(age);
+
+
+       expect(galacticCalulation.age).toEqual(age)
+     })
+   })
+   describe('GalacticCalculator', function() {
+       //first test to pass
+       it('should expect to pass/if it returns correct value of the age in mercury years', function() {
+         let age = 35;
+         let galCal = new GalacticCalculator(age);
+         let mercury = age / .22;
+
+        expect(mercury).toEqual(159.0909090909091);
+
+       })
+     })
+     describe('GalacticCalculator', function() {
+         //first test to pass
+         it('should expect to pass/if it returns correct value of the age in venus years', function() {
+           let age = 35;
+           let galCal = new GalacticCalculator(age);
+           let mercury = age / .22;
+           let venus= age / .62;
+
+           console.log(venus);
+
+
+
+
+          expect(venus).toEqual(56.45161290322581);
+
+         })
+       })
+       describe('GalacticCalculator', function() {
+           //first test to pass
+           it('should expect to pass/if it returns correct value of the age in mercury years', function() {
+             let age = 35;
+             let galCal = new GalacticCalculator(age);
+             let mercury = age / .22;
+             let venus = age / .62;
+             let mars = age / 1.88;
+             let jupiter = age / 11.86;
+
+
+             console.log(mars);
+
+
+
+            expect(mars).toEqual(18.617021276595747);
+
+           })
+         })
+         describe('GalacticCalculator', function() {
+             //first test to pass
+             it('should expect to pass/if it returns correct value of the age in mercury years', function() {
+               let age = 35;
+               let galCal = new GalacticCalculator(age);
+               let mercury = age / .22;
+               let venus = age / .62;
+               let mars = age / 1.88;
+               let jupiter = age / 11.86;
+               console.log(jupiter);
+
+              expect(jupiter).toEqual(2.9510961214165263);
+
+             })
+           })
--- src/galactic-cal.js
+++ src/galactic-cal.js
@@ -1,7 +1,24 @@
 // Back-end
 export class GalacticCalculator {
-  constructor(age){
+  constructor(age,mercury,venus,mars,jupiter){
     this.age = age;
+    this.mercury = mercury;
+    this.venus = venus;
+    this.mars = mars;
+    this.jupiter = jupiter;
+}
+  mercuryCal() {
+     this.mercury = this.age / .22;
+}
+  venusCal() {
+    this.venus = this.age / .62;
+}
+  marsCal() {
+    this.mars = this.age / 1.88;
+}
+  jupiterCal() {
+    this.jupiter = this.age / 11.86;
+}
 
-  }
-};  
+
+};
